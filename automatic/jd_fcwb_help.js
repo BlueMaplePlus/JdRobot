e0590ae9eef78679016e5b7d7607f62deafd9b0d
LiuZXBIZ <999955@qq.com>
2022-06-07 16:37:05 +0800

    Update jd_fcwb_help.js

--- automatic/jd_fcwb_help.js
+++ automatic/jd_fcwb_help.js
@@ -3,9 +3,8 @@
 活动入口：极速版-发财挖宝
 默认助力满30停止。
 定时自己按需设置，默认不运行
-40 10 10 5 * https://github.com/6dylan6/jdpro/jd_fcwb_help.js
+40 6,17 * * * https://github.com/6dylan6/jdpro/jd_fcwb_help.js
 
-cron "40 6,17 * * *" 
 
 */
 const $ = new Env('发财挖宝助力');
